14db10400b4b9a3bb1417b951149b057e6bd9179
Gabriele Napoli <gabriele.nap@gmail.com>
2023-01-02 23:57:34 +0100

    upd importer

--- importer/index.js
+++ importer/index.js
@@ -19,22 +19,22 @@ const { Saga } = require("./Saga");
 
   // VOLUMES
 
-  // console.log("Deleting all volumes...");
-  // await Volume.deleteMany({});
+  console.log("Deleting all volumes...");
+  await Volume.deleteMany({});
 
-  // console.log("Insert all volumes...");
+  console.log("Insert all volumes...");
 
-  // try {
-  //   for (const v of volumes) {
-  //     const { number, title, chapters, summary } = v;
-  //     const volume = new Volume({ number, title, chapters, summary });
-  //     await volume.save();
+  try {
+    for (const v of volumes) {
+      const { number, title, chapters, summary } = v;
+      const volume = new Volume({ number, title, chapters, summary });
+      await volume.save();
 
-  //     console.log(`Saved volume ${number}`);
-  //   }
-  // } catch (err) {
-  //   console.log("Error!", err);
-  // }
+      console.log(`Saved volume ${number}`);
+    }
+  } catch (err) {
+    console.log("Error!", err);
+  }
 
   // CHAPTER
 
@@ -75,54 +75,54 @@ const { Saga } = require("./Saga");
 
   // EPISODES
 
-  // console.log("Deleting all episodes...");
-  // await Episode.deleteMany({});
+  console.log("Deleting all episodes...");
+  await Episode.deleteMany({});
 
-  // console.log("Insert all episodes...");
+  console.log("Insert all episodes...");
 
-  // try {
-  //   for (const e of episodes) {
-  //     const { title, number, release, summary, chapters, image } = e;
-  //     const episode = new Episode({
-  //       title,
-  //       number,
-  //       release,
-  //       summary,
-  //       chapters,
-  //       image,
-  //     });
-  //     await episode.save();
+  try {
+    for (const e of episodes) {
+      const { title, number, release, summary, chapters, image } = e;
+      const episode = new Episode({
+        title,
+        number,
+        release,
+        summary,
+        chapters,
+        image,
+      });
+      await episode.save();
 
-  //     console.log(`Saved episode ${number}`);
-  //   }
-  // } catch (err) {
-  //   console.log("Error!", err);
-  // }
+      console.log(`Saved episode ${number}`);
+    }
+  } catch (err) {
+    console.log("Error!", err);
+  }
 
   // SAGAS
 
-  // console.log("Deleting all sagas...");
-  // await Saga.deleteMany({});
+  console.log("Deleting all sagas...");
+  await Saga.deleteMany({});
 
-  // console.log("Insert all sagas...");
+  console.log("Insert all sagas...");
 
-  // try {
-  //   for (const s of sagas) {
-  //     const { name, summary, chapters, episodes, volumes } = s;
-  //     const saga = new Saga({
-  //       name,
-  //       summary,
-  //       chapters,
-  //       episodes,
-  //       volumes,
-  //     });
-  //     await saga.save();
+  try {
+    for (const s of sagas) {
+      const { name, summary, chapters, episodes, volumes } = s;
+      const saga = new Saga({
+        name,
+        summary,
+        chapters,
+        episodes,
+        volumes,
+      });
+      await saga.save();
 
-  //     console.log(`Saved saga ${name}`);
-  //   }
-  // } catch (err) {
-  //   console.log("Error!", err);
-  // }
+      console.log(`Saved saga ${name}`);
+    }
+  } catch (err) {
+    console.log("Error!", err);
+  }
 
   console.log("END");
 })();
